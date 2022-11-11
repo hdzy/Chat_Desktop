73052717f1d1e5d21eae083ace27884fd775c2b2
hdzylovesyou <smirnov.oleg.1231@gmail.com>
2022-11-11 16:23:29 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const element = document.querySelectorAll('img')
+let oldElement =1;
+let activeChecker = document.getElementById("activeChecker");
+let temp;
+
+for (i=0; i<7; i++) {
+    element[i].addEventListener('click', addClassActive(i));
+}
+
+function addClassActive(getElem) {
+    return function () {
+        if (element[getElem]!==oldElement) {
+            $(element[getElem]).addClass('active');
+            $(element[oldElement]).removeClass('active');
+            oldElement = getElem;
+            switch (getElem) {
+                case 1:
+                    temp=63; //4.5
+                    break;
+                case 2:
+                    temp=136;
+                    break;
+                case 3:
+                    temp=208;
+                    break;
+                case 4:
+                    temp=281;
+                    break;
+                case 5:
+                    temp=354;
+                    break;
+            }
+            activeChecker.style.top=temp+"px";
+        }
+    }
+}
